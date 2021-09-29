ac84d074e9ece6dba40b606c34bfbac9e982f81b
Alastair Pandelus <alastair.pandelus@angloamerican.com>
2021-09-29 11:32:10 +0100

    Added Microsoft Azure AD Authentication Auth values held in environment variables

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -25,11 +25,30 @@ import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInProviderConfig,
+  SignInPage,
+} from '@backstage/core-components';
 import { createApp, FlatRoutes } from '@backstage/core-app-api';
+
+import { microsoftAuthApiRef } from '@backstage/core-plugin-api';
+
+const microsoftProvider: SignInProviderConfig = {
+  id: 'microsoft-auth-provider',
+  title: 'Microsoft',
+  message: 'Sign In using Microsoft Azure AD',
+  apiRef: microsoftAuthApiRef,
+};
 
 const app = createApp({
   apis,
+  components: {
+    SignInPage: props => {
+      return <SignInPage {...props} providers={['guest', microsoftProvider]} />;
+    },
+  },
   bindRoutes({ bind }) {
     bind(catalogPlugin.externalRoutes, {
       createComponent: scaffolderPlugin.routes.root,
